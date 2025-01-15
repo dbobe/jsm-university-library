54c03a5a683493cf7ed66078506ee67241920dc7
dbobe <bobe.david@live.com>
2025-01-14 20:36:23 -0500

    Add functional email

--- src/lib/workflow.ts
+++ src/lib/workflow.ts
@@ -27,7 +27,7 @@ export const sendEmail = async ({
       provider: resend({ token: config.env.resendToken }),
     },
     body: {
-      from: "David Bobe <contact@davidbobe.com>",
+      from: "David Bobe <david@davidbobe.com>",
       to: [email],
       subject,
       html: message,
