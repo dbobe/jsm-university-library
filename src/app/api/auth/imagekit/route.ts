5adefdd3f0a20a9d22d9f106ec8319f5dcb46e8b
dbobe <bobe.david@live.com>
2025-01-14 20:00:38 -0500

    Trying to fix CORS error on image upload

--- src/app/api/auth/imagekit/route.ts
+++ src/app/api/auth/imagekit/route.ts
@@ -11,5 +11,24 @@ const imagekit = new ImageKit({
 });
 
 export async function GET() {
-  return NextResponse.json(imagekit.getAuthenticationParameters());
+  return NextResponse.json(imagekit.getAuthenticationParameters(), {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
+
+export async function OPTIONS() {
+  return NextResponse.json(
+    {},
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    },
+  );
 }
