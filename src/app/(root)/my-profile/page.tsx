cb830afd9e5e8e5fe7e4c280ff0dcbc451fb1e72
dbobe <bobe.david@live.com>
2025-01-16 20:40:07 -0500

    Profile page update with dropdown menu and borrowed books

--- src/app/(root)/my-profile/page.tsx
+++ src/app/(root)/my-profile/page.tsx
@@ -1,22 +1,43 @@
-import { signOut } from "@/auth";
+import { auth, signOut } from "@/auth";
 import BookList from "@/components/BookList";
 import { Button } from "@/components/ui/button";
 import { sampleBooks } from "@/constants";
+import { db } from "@/database/db";
+import { and, eq } from "drizzle-orm";
+import { books, borrowRecords } from "@/database/schema";
 
-export default function MyProfile() {
+export default async function MyProfile() {
+  const session = await auth();
+
+  const borrowedBooks = await db
+    .select({
+      id: books.id,
+      title: books.title,
+      author: books.author,
+      genre: books.genre,
+      rating: books.rating,
+      totalCopies: books.totalCopies,
+      availableCopies: books.availableCopies,
+      description: books.description,
+      coverColor: books.coverColor,
+      coverUrl: books.coverUrl,
+      videoUrl: books.videoUrl,
+      summary: books.summary,
+      createdAt: books.createdAt,
+    })
+    .from(borrowRecords)
+    .innerJoin(books, eq(borrowRecords.bookId, books.id))
+    .where(
+      and(
+        eq(borrowRecords.userId, session?.user?.id as string),
+        eq(borrowRecords.status, "BORROWED"),
+      ),
+    );
+
+  console.log("🚀 ~ MyProfile ~ borrowedBooks:", borrowedBooks);
   return (
     <>
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-        className="mb-10"
-      >
-        <Button>Logout</Button>
-      </form>
-
-      <BookList title="Borrowed Books" books={sampleBooks} />
+      <BookList title="Borrowed Books" books={borrowedBooks} />
     </>
   );
 }
